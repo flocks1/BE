de3175bb7928c40f1eca374e0f0c237c1368d543
Thorben Bender <bender.thorben0@gmail.com>
2019-04-15 20:41:06 +0200

    server testing working and added cross-env

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const server = require('./server/server');
+
 
 server.listen(4000, () => {
     console.log('Server listening on Port 4000');
--- server/server.test.js
+++ server/server.test.js
@@ -2,7 +2,7 @@ const server = require('./server');
 const request = require('supertest');
 
 describe('server', () => {
-    describe('is the right enviroment', () => {
+    it('is the right enviroment', () => {
         expect(process.env.DB_ENV).toBe('testing');
     })
 })
