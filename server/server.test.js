11b0675a9f356c2226e92d43b0ddbec337e5fdc6
Thorben Bender <bender.thorben0@gmail.com>
2019-04-15 20:25:59 +0200

    created server test file

---
+++ server/server.test.js
@@ -0,0 +1,8 @@
+const server = require('./server');
+const request = require('supertest');
+
+describe('server', () => {
+    describe('is the right enviroment', () => {
+        expect(process.env.DB_ENV).toBe('testing');
+    })
+})
