30b797b2e76254985e207a886d47162659e78b34
Thorben Bender <bender.thorben0@gmail.com>
2019-04-16 11:27:26 +0200

    use bcrypt in the register helper file instead of the route file

--- api/Routes/register/registerRoute.js
+++ api/Routes/register/registerRoute.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const register = require('./registerRouteHelper');
-const bcrypt = require('bcryptjs');
 
 const route = express.Router();
 
 route.post('', async (req, res) => {
     try {
-        const credentials = req.body;
-        hash = bcrypt.hashSync(credentials.password, 10);
-        credentials.password = hash;
         user = await register(credentials);
         if (user) {
             res.status(200).json(user);
@@ -17,3 +13,5 @@ route.post('', async (req, res) => {
         res.status(500).json(error);
     }
 })
+
+module.exports = route;
--- api/Routes/register/registerRouteHelper.js
+++ api/Routes/register/registerRouteHelper.js
@@ -1,7 +1,11 @@
 const db = require('../data/dbConfig.js');
+const bcrypt = require('bcryptjs');
 
 module.exports = register;
 
-async function register(user) {
+async function register(credentials) {
+    user = credentials;
+    hash = bcrypt.hashSync(credentials.password, 10);
+    user.password = hash;
     return db('Users').insert(user);
 }
