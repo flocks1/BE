57fd88d239ce233cfe29e165e82dcef53582fc6b
Thorben Bender <bender.thorben0@gmail.com>
2019-04-16 11:33:42 +0200

    connected server with the registerRoute

--- server/server.js
+++ server/server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+//Routes
+const registerRoute = require('../api/Routes/register/registerRoute');
+
 const server = express();
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
+
+server.use('/register', registerRoute);
+
 
 module.exports = server;
