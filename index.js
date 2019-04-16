a09924eb22993e0a7b697c7a1bbfbd47610c5b12
Thorben Bender <bender.thorben0@gmail.com>
2019-04-16 18:00:45 +0200

    change the port

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const server = require('./server/server');
 
+port = process.env.PORT || 8000
 
-server.listen(4000, () => {
-    console.log('Server listening on Port 4000');
+
+server.listen(port, () => {
+    console.log('Server listening on Port 8000');
 })
